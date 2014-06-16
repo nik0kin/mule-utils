f25fc02ccb2900b340d6d6c9f32057ff0e8ff645
Nikolas Poklitar <nikolas.poklitar@gmail.com>
2014-06-16 18:39:37 -0500

    moved actions utils here

---
+++ actionsUtils.js
@@ -0,0 +1,24 @@
+var _ = require('lodash');
+
+exports.searchThruSpacesForId = function (spaces, spaceId) {
+  var found = false;
+  _.each(spaces, function (value, key) {
+    if (value.boardSpaceId === spaceId) {
+      found = value;
+    }
+  });
+  return found;
+};
+
+exports.searchThruPiecesForId = function (pieces, pieceId) {
+  if (typeof pieceId === 'string') {
+    pieceId = parseInt(pieceId);
+  }
+  var found = false;
+  _.each(pieces, function (value, key) {
+    if (value.id === pieceId) {
+      found = value;
+    }
+  });
+  return found;
+};
---
+++ randomUtils.js
@@ -0,0 +1,15 @@
+var _ = require('lodash');
+
+// from http://stackoverflow.com/questions/2380019/generate-8-unique-random-numbers-between-1-and-100
+exports.pickRandom = function (array, picks) {
+  array = _.clone(array);
+
+  for (i = array.length-1; i > 1  ; i--) {
+    var r = Math.floor(Math.random()*i);
+    var t = array[i];
+    array[i] = array[r];
+    array[r] = t;
+  }
+
+  return array.slice(0,picks);
+};
